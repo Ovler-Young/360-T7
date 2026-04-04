8b267b1e1ebe47883089b25225016b2c35ab406a
Ovler <chengyujin86@gmail.com>
2026-04-04 08:48:58 +0000

    fix: ip address

--- 2410-diy-part2.sh
+++ 2410-diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/ImmortalWrt/ImmortalWrt-24.10-$(shell TZ="America/New_York" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
